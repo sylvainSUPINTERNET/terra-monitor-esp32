40096ed6e31736ee39a9fc51423f8862a2479389
sylvainSUPINTERNET <sylvain.joly00@gmail.com>
2025-03-05 00:20:14 +0100

    FK BLE

--- src/main.cpp
+++ src/main.cpp
@@ -8,70 +8,147 @@
 #include <Adafruit_Sensor.h>
 #include <Adafruit_BME280.h>
 #include "WiFiCredentialsCallbacks.h"
+#include <WebServer.h>
 
-#define SDA_PIN 17
-#define SCL_PIN 18
+WebServer server(80);
 
+void handleConfig() {
+  if (server.hasArg("ssid") && server.hasArg("password")) {
+    String ssid = server.arg("ssid");
+    String password = server.arg("password");
+    
+    Serial.println("SSID: " + ssid);
+    //Serial.println("Password: " + password);
 
-Adafruit_BME280 bme; // I2C
+    server.send(200, "text/plain", "OK");
 
+    delay(1000);
+
+    WiFi.softAPdisconnect(true);
+    WiFi.mode(WIFI_STA);
+    WiFi.begin(ssid.c_str(), password.c_str());    
+  } else {
+    server.send(400, "text/plain", "Bad Request");
+  }
+}
 
 void setup() {
-  const std::string MANDATORY_PREFIX = "TERRA-IOT";
   Serial.begin(115200);
   while(!Serial);
 
-  Serial.println("Start...");
+  WiFi.mode(WIFI_AP);
+  WiFi.softAP("ESP32_SETUP", "12345678");
+  Serial.println("AP WIFI");
+
+  Serial.print("IP ESP32 : ");
+  Serial.println(WiFi.softAPIP());
 
 
-  // if (!bme.begin()) { 
-  //   Serial.println("Could not find a valid BME280 sensor, check wiring!");
-  // }
+  server.on("/setup", HTTP_POST, handleConfig);
+  server.on("/", HTTP_GET, [](){
 
+    const char* html_page =
+        "<!DOCTYPE html>"
+        "<html>"
+        "<head><title>ESP32 Setup</title></head>"
+        "<body>"
+        "<h1>Bienvenue sur l'ESP32</h1>"
+        "<form action=\"/config\" method=\"post\">"
+        "SSID: <input type=\"text\" name=\"ssid\"><br>"
+        "Password: <input type=\"password\" name=\"password\"><br>"
+        "<input type=\"submit\" value=\"Envoyer\">"
+        "</form>"
+        "</body>"
+        "</html>";
 
-  Serial.println("init BLE service...");
-  BLEDevice::init(std::string(MANDATORY_PREFIX) + std::string("ESP32"));
-  BLEServer *pServer = BLEDevice::createServer();
+    server.send(200, "text/html", html_page);
+  });
 
-  pServer->setCallbacks(new BLEServerCallbacks()); // TODO : add callback ?
+  server.begin();
+  Serial.println("Web server started");
 
-  const char* serviceWiFiCredsUUID = "4fafc201-1fb5-459e-8fcc-c5c9c331914b";
-  const char* characteristicWiFiCredsUUID = "6e400002-b5a3-f393-e0a9-e50e24dcca9e";
-
-  BLEService *pServicePairing = pServer->createService(serviceWiFiCredsUUID);
-  BLECharacteristic *pCharacteristicPairing = pServicePairing->createCharacteristic(
-                                         characteristicWiFiCredsUUID,
-                                         BLECharacteristic::PROPERTY_WRITE | BLECharacteristic::PROPERTY_NOTIFY);
-  pCharacteristicPairing->setCallbacks(new WiFiCredentialsCallbacks(&Serial));
-  pServicePairing->start();
-
-  BLEAdvertising *pAdvertisingPairing = pServer->getAdvertising();
-  pAdvertisingPairing->start();
-
-  Serial.println("Service : " + String(serviceWiFiCredsUUID));
-  Serial.println("Characteristic : " + String(characteristicWiFiCredsUUID));
-  Serial.println("Characteristic defined! Now you can read/write it in your phone");
-
-
-
-  /*
-  BLEService *pService = pServer->createService("180A");
-  BLECharacteristic *pCharacteristic = pService->createCharacteristic(
-                                         "2A23",
-                                         BLECharacteristic::PROPERTY_READ
-                                       );
-  pCharacteristic->setValue("Hello World");
-  pService->start();
-
-  BLEAdvertising *pAdvertising = pServer->getAdvertising();
-  pAdvertising->start();
-  */
   
 }
 
 void loop() {
-
+  server.handleClient();
 }
+
+// #define SDA_PIN 17
+// #define SCL_PIN 18
+
+// Adafruit_BME280 bme; // I2C
+
+
+// class MyServerCallbacks: public BLEServerCallbacks {
+//   void onConnect(BLEServer* pServer) {
+//     Serial.println("✅ Client connected");
+//   };
+
+//   void onDisconnect(BLEServer* pServer) {
+//     Serial.println("❌ Client disconnected");
+//     pServer->getAdvertising()->start();
+//   }
+// };
+
+
+
+// void setup() {
+//   const std::string MANDATORY_PREFIX = "TERRA-IOT";
+//   Serial.begin(115200);
+//   while(!Serial);
+
+//   Serial.println("Start...");
+
+
+//   // if (!bme.begin()) { 
+//   //   Serial.println("Could not find a valid BME280 sensor, check wiring!");
+//   // }
+
+
+//   Serial.println("init BLE service ...");
+//   BLEDevice::init(std::string(MANDATORY_PREFIX) + std::string("ESP32"));
+//   BLEServer *pServer = BLEDevice::createServer();
+
+//   pServer->setCallbacks(new MyServerCallbacks());
+
+//   const char* serviceWiFiCredsUUID = "4fafc201-1fb5-459e-8fcc-c5c9c331914b";
+//   const char* characteristicWiFiCredsUUID = "6e400002-b5a3-f393-e0a9-e50e24dcca9e";
+
+//   BLEService *pServicePairing = pServer->createService(serviceWiFiCredsUUID);
+//   BLECharacteristic *pCharacteristicPairing = pServicePairing->createCharacteristic(
+//                                          characteristicWiFiCredsUUID,
+//                                          BLECharacteristic::PROPERTY_WRITE | BLECharacteristic::PROPERTY_NOTIFY);
+//   pCharacteristicPairing->setCallbacks(new WiFiCredentialsCallbacks(&Serial));
+//   pServicePairing->start();
+//   delay(500);
+  
+//   BLEAdvertising *pAdvertisingPairing = pServer->getAdvertising();
+//   pAdvertisingPairing->start();
+
+//   Serial.println("Service : " + String(serviceWiFiCredsUUID));
+//   Serial.println("Characteristic : " + String(characteristicWiFiCredsUUID));
+//   Serial.println("Characteristic defined! Now you can read/write it in your phone");
+
+
+//   /*
+//   BLEService *pService = pServer->createService("180A");
+//   BLECharacteristic *pCharacteristic = pService->createCharacteristic(
+//                                          "2A23",
+//                                          BLECharacteristic::PROPERTY_READ
+//                                        );
+//   pCharacteristic->setValue("Hello World");
+//   pService->start();
+
+//   BLEAdvertising *pAdvertising = pServer->getAdvertising();
+//   pAdvertising->start();
+//   */
+  
+// }
+
+// void loop() {
+
+// }
 
 
 
